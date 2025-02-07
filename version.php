3a8bdf64afcdb7c66d5d189f7d42e76b1be8a76a
Jhon Rangel <jrangelardila@gmail.com>
2025-02-07 18:23:39 -0500

    Update the version.php

--- version.php
+++ version.php
@@ -23,6 +23,6 @@
  */
 $plugin->component = 'tool_category_backup';
 $plugin->release = '1.0 jrangelardila';
-$plugin->version = 2025020600;
+$plugin->version = 2025020700;
 $plugin->requires = 2023042400;
 $plugin->maturity = MATURITY_STABLE;
